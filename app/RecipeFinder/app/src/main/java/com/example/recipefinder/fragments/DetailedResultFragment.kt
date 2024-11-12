4fa9eb2bc30f0cc2d77b1dc81fa5289b21e8d536
Jannis <jannism87@gmail.com>
2024-11-12 18:19:53 +0100

    initial commit

---
+++ app/RecipeFinder/app/src/main/java/com/example/recipefinder/fragments/DetailedResultFragment.kt
@@ -0,0 +1,6 @@
+package com.example.recipefinder.fragments
+
+import androidx.fragment.app.Fragment
+
+class DetailedResultFragment : Fragment() {
+}
---
+++ app/RecipeFinder/app/src/main/java/com/example/recipefinder/fragments/SearchFragment.kt
@@ -0,0 +1,17 @@
+package com.example.recipefinder.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.recipefinder.databinding
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentR? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}

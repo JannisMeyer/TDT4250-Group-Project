7a88b5a8835d2053a278d028373fe859a55f872a
Jannis <jannism87@gmail.com>
2024-11-15 19:46:13 +0100

    reinitialized app/new project

---
+++ app/RecipeExplorer/app/src/main/java/com/example/recipeexplorer/fragments/ResultsfFragment.kt
@@ -0,0 +1,6 @@
+package com.example.recipeexplorer.fragments
+
+import androidx.fragment.app.Fragment
+
+class ResultsFragment : Fragment() {
+}
---
+++ app/RecipeExplorer/app/src/main/java/com/example/recipeexplorer/fragments/SearchFragment.kt
@@ -0,0 +1,17 @@
+package com.example.recipeexplorer.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+//import com.example.recipefinder
+
+class SearchFragment : Fragment() {
+
+    //private var _binding: ? = null
+    //private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}

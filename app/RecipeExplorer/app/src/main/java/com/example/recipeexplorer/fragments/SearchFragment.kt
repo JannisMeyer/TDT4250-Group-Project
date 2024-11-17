6fe8a9f5e9a67434adb7aaf312082cbfd79a53d0
Jannis <jannism87@gmail.com>
2024-11-17 13:49:22 +0100

    include data binding

--- app/RecipeExplorer/app/src/main/java/com/example/recipeexplorer/fragments/SearchFragment.kt
+++ app/RecipeExplorer/app/src/main/java/com/example/recipeexplorer/fragments/SearchFragment.kt
@@ -2,12 +2,12 @@ package com.example.recipeexplorer.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-//import com.example.recipefinder
+import com.example.recipeexplorer.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
 
-    //private var _binding: ? = null
-    //private val binding get() = _binding!!
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
